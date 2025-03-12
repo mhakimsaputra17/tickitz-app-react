54a9449d3ecfde6fafb828e5f625b22c076ddde5
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-12 22:29:46 +0700

    feat(home): add HeroSection and FeatSection components to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../../components/layout/Navbar/Navbar";
 
+import HeroSection from "../../components/ui/Hero/HeroSection.jsx";
+import FeatSection from "../../components/ui/Features/FeatSection.jsx";
+
 function Home() {
   return (
     <>
       <Navbar />
+      <HeroSection />
+      <FeatSection />
     </>
   );
 }
