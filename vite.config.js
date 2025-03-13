a424df964c1cc5126e6ee6434e60faf5bb3a55c0
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-13 15:38:25 +0700

    feat(env): add .env file for storing TMDB API key and update Vite config to use it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    "process.env.TMDB_API_KEY": JSON.stringify(process.env.TMDB_API_KEY),
+  },
 });
