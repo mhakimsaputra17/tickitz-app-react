5f6d2be6bc11f49fc896e650ec7c585ee2fe0363
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-13 22:54:30 +0700

    feat(routing): implement routing for movies and movie details with SharedLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Home from "./pages/Home/Home";
-
+import { Route, Routes, BrowserRouter } from "react-router";
+import SharedLayout from "./routes/SharedLayout";
+import Movies from "./pages/movies/Movies";
+import MovieDetail from "./pages/movies/MovieDetail";
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="movies">
+              <Route index element={<Movies />} />
+              <Route path="detail/:movieId" element={<MovieDetail />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
