2e6c1854702980ee68ecb65be63e5a3eeba70a38
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-13 22:54:44 +0700

    feat(navbar): replace anchor tags with Link components for client-side navigation

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -12,59 +12,59 @@ const Navbar = () => {
       <nav className="flex justify-between items-center px-[5%] py-5 border-b border-gray-200 bg-white">
         {/* Logo */}
         <div className="flex-1 max-w-[130px]">
-          <a href="/">
+          <Link to="/">
             <img
               className="w-full h-auto"
-              src="src\assets\icons\tickets.svg"
+              src="https://raw.githubusercontent.com/mhakimsaputra17/weekly-task3/7c2e4c1f0b29e0b3de07e412c7c20490a272f31d/assets/images/logo/tickets.svg"
               alt="logo-tickitz"
             />
-          </a>
+          </Link>
         </div>
 
         {/* Nav Menu - Desktop */}
         <div className="hidden md:flex flex-4 justify-center">
           <ul className="flex gap-10">
             <li>
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="text-[#0f172a] text-sm font-normal relative hover:text-blue-primary after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-blue-primary after:bottom-[-5px] after:left-0 after:transition-all after:duration-300 hover:after:w-full"
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/movies"
+              <Link
+                to="/movies"
                 className="text-[#0f172a] text-sm font-normal relative hover:text-blue-primary after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-blue-primary after:bottom-[-5px] after:left-0 after:transition-all after:duration-300 hover:after:w-full"
               >
                 Movie
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/order"
+              <Link
+                to="/order"
                 className="text-[#0f172a] text-sm font-normal relative hover:text-blue-primary after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-blue-primary after:bottom-[-5px] after:left-0 after:transition-all after:duration-300 hover:after:w-full"
               >
                 Buy Ticket
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
 
         {/* Auth Buttons - Desktop */}
         <div className="hidden md:flex flex-1 justify-end gap-3">
-          <a
-            href="/auth/signin"
+          <Link
+            to="/auth/signin"
             className="px-[18px] py-3 rounded border  border-blue-700 text-blue-primary text-sm font-normal tracking-wider hover:bg-blue-50 transition-colors"
           >
             Sign in
-          </a>
-          <a
-            href="/auth/signup"
+          </Link>
+          <Link
+            to="/auth/signup"
             className="px-[18px] py-3 rounded bg-blue-600 border border-blue-700 text-white text-sm font-normal tracking-wider hover:bg-blue-700 transition-colors"
           >
             Sign Up
-          </a>
+          </Link>
         </div>
 
         {/* Hamburger Menu - Mobile */}
