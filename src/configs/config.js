f41d860228a38d21cf19c16b7083e1c0d3896337
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-15 02:29:00 +0700

    feat(config): update API key handling and add new TMDB API endpoints

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,14 @@
 export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-export const API_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTM3NTM1YTkwNzk5YWQzNGUzYzI1MWRkZDAyODVkNSIsIm5iZiI6MTc0MTM1NjEwNS4wMDgsInN1YiI6IjY3Y2FmYzQ5NTQ3ODNjYWFhM2FmZjI4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TRKDbK0_ae0jY-ZYpujQnuIvG90lefKw8fFul9qWdJM";
+export const TMDB_API_KEY = import.meta.env.VITE_API_KEY;
+export const API_URL_POPULAR =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=6";
+export const API_URL_TOP_RATED =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=6";
+export const API_URL_UPCOMING =
+  "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=2";
+export const API_URL_NOW_PLAYING =
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=6";
+export const API_URL_GENRE =
+  "https://api.themoviedb.org/3/genre/movie/list?language=en-US";
+export const API_URL_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?language=en-US&query=";
