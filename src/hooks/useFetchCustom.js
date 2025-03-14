0348d382e19cb34d8d98d4491f920d52bda50534
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-15 02:29:15 +0700

    feat(hooks): add custom useFetch hook for improved data fetching with TMDB API

---
+++ src/hooks/useFetchCustom.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { BASE_IMAGE_URL, TMDB_API_KEY } from "../configs/config";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${TMDB_API_KEY}`,
+  },
+};
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`Http error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json.results);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
