86a949dbc24eb18c307d9886d6bb1dd7e651c0da
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-15 02:26:49 +0700

    feat(movies): update MovieTags component to include Fantasy tag and enhance styling

--- src/components/common/MovieTags.jsx
+++ src/components/common/MovieTags.jsx
@@ -4,11 +4,14 @@ function MovieTags() {
   return (
     <>
       <div className="tags flex gap-[10px] mt-[14px] flex-wrap">
-        <span className="tag bg-[rgba(160,_163,_189,_0.1)] rounded-[20px] text-[11px] text-center font-normal leading-[normal] px-[15px] py-[5px]">
+        <span className="tag text-[#a0a3bd] bg-[rgba(160,_163,_189,_0.1)] rounded-[20px] text-[11px] text-center font-normal leading-[normal] px-[15px] py-[5px]">
           Action
         </span>
-        <span className="tag bg-[rgba(160,_163,_189,_0.1)] rounded-[20px] text-[11px] text-center font-normal leading-[normal] px-[15px] py-[5px]">
+        <span className="tag text-[#a0a3bd] bg-[rgba(160,_163,_189,_0.1)] rounded-[20px] text-[11px] text-center font-normal leading-[normal] px-[15px] py-[5px]">
           Adventure
+        </span>
+        <span className="tag text-[#a0a3bd] bg-[rgba(160,_163,_189,_0.1)] rounded-[20px] text-[11px] text-center font-normal leading-[normal] px-[15px] py-[5px]">
+          Fantasy
         </span>
       </div>
     </>
