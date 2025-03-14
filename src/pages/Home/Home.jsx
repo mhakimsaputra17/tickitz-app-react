051ef4a693776f5aa19d5e33745bd55fbbf3bcbd
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-14 11:39:57 +0700

    feat(home): add MoviesUpcoming component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/layout/Navbar/Navbar.jsx";
 import HeroSection from "../../components/ui/Hero/HeroSection.jsx";
 import FeatSection from "../../components/ui/Features/FeatSection.jsx";
 import MoviesHome from "../../components/ui/Movies/MoviesHome.jsx";
+import MoviesUpcoming from "../../components/ui/Movies/MoviesUpcoming.jsx";
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
       <HeroSection />
       <FeatSection />
       <MoviesHome />
+      <MoviesUpcoming />
     </>
   );
 }
