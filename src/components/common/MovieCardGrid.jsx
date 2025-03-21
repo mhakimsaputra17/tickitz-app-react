06a52bde6facb5df3a59debbf8c6c8aa598a2bfd
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-22 00:35:41 +0700

    feat(movie): conditionally display recommendation badge based on vote average

--- src/components/common/MovieCardGrid.jsx
+++ src/components/common/MovieCardGrid.jsx
@@ -11,6 +11,7 @@ function MovieCardGrid({
   imageUrl,
   title,
   onButtonClick,
+  vote_average,
 }) {
   const {
     data: movieDetail,
@@ -28,10 +29,11 @@ function MovieCardGrid({
         className="movie-card mb-7 transition-transform ease-in-out duration-300 hover:translate-y-[-10px]"
       >
         <div className="poster relative overflow-hidden rounded-lg shadow-md">
-          <div className="badge-recommendation absolute top-2 left-2 rounded-xl rounded-bl-none bg-blue-800 text-white px-2 py-1 text-xs font-semibold flex items-center gap-2 z-10 shadow-md animate-pulse">
-            Recommendation
-          </div>
-
+          {vote_average > 8 && (
+            <div className="badge-recommendation absolute top-2 left-2 rounded-xl rounded-bl-none bg-blue-800 text-white px-2 py-1 text-xs font-semibold flex items-center gap-2 z-10 shadow-md animate-pulse">
+              Recommendation
+            </div>
+          )}
           <img
             src={imageUrl}
             alt={title}
