9ffd3227b506573f21b160abc76f044b20b57108
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-24 00:24:08 +0700

    feat(store): refactor store configuration to use combineReducers for better scalability

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import localstorage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage";
+
+// import reducers
+import authReducer from "./slices/authSlice";
 
 const persistConfig = {
   key: "root",
-  storage: localstorage,
+  storage,
   whitelist: ["auth"],
 };
 
-const persistedReducer = persistReducer(persistConfig, authReducer);
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  // Using default middleware (no thunk)
+});
+
+export const persistor = persistStore(store);
