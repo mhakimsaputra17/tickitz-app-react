4fc971969b674cab8700ad2be6118697e6f0abed
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-24 00:23:46 +0700

    feat(app): integrate Redux store and add ProtectedRoute for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -13,61 +13,80 @@ import Ticket from "./pages/order/Ticket";
 import OrderHistory from "./pages/profile/OrderHistory";
 import ListMovie from "./pages/admin/ListMovie";
 import NewMovie from "./pages/admin/NewMovie";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<SharedLayout />}>
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route index element={<Home />} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<SharedLayout />}>
+                <Route path="/home" element={<Navigate to="/" replace />} />
+                <Route index element={<Home />} />
 
-            <Route path="movies">
-              <Route index element={<Movies />} />
-              <Route path="detail/:movieId" element={<MovieDetail />} />
-            </Route>
+                <Route path="movies">
+                  <Route index element={<Movies />} />
+                  <Route
+                    path="detail/:movieId"
+                    element={
+                      <ProtectedRoute>
+                        <MovieDetail />
+                      </ProtectedRoute>
+                    }
+                  />
+                </Route>
 
-            <Route path="order">
-              <Route index element={<Order />} />
-              <Route path=":movieId" element={<Order />} />
-              <Route path=":movieId/:orderId" element={<Order />} />
-              <Route path="payment" element={<Payment />} />
-              <Route path="ticket" element={<Ticket />} />
-            </Route>
+                <Route path="order">
+                  <Route index element={<Order />} />
+                  <Route path=":movieId" element={<Order />} />
+                  <Route path=":movieId/:orderId" element={<Order />} />
+                  <Route path="payment" element={<Payment />} />
+                  <Route path="ticket" element={<Ticket />} />
+                </Route>
 
-            <Route path="profile">
-              <Route index element={<Navigate to="/profile/order" replace />} />
-              <Route path="order" element={<Order />} />
-              <Route path="order/history" element={<OrderHistory />} />
-            </Route>
-            <Route path="admin">
+                <Route path="profile">
+                  <Route
+                    index
+                    element={<Navigate to="/profile/order" replace />}
+                  />
+                  <Route path="order" element={<Order />} />
+                  <Route path="order/history" element={<OrderHistory />} />
+                </Route>
+                <Route path="admin">
+                  <Route
+                    index
+                    element={<Navigate to="/admin/dashboard" replace />}
+                  />
+                  <Route path="movie/list" element={<ListMovie />} />
+                  <Route path="movie/add" element={<NewMovie />} />
+                </Route>
+              </Route>
+              <Route path="/auth" element={<AuthLayout />}>
+                e
+                <Route index element={<Navigate to="/auth/signin" rplace />} />
+                <Route path="signin" element={<SignIn />} />
+                <Route path="signup" element={<SignUp />} />
+              </Route>
+              /*{" "}
               <Route
-                index
-                element={<Navigate to="/admin/dashboard" replace />}
-              />
-              <Route path="movie/list" element={<ListMovie />} />
-              <Route path="movie/add" element={<NewMovie />} />
-            </Route>
-          </Route>
-          <Route path="/auth" element={<AuthLayout />}>
-            e
-            <Route index element={<Navigate to="/auth/signin" rplace />} />
-            <Route path="signin" element={<SignIn />} />
-            <Route path="signup" element={<SignUp />} />
-          </Route>
-          /*{" "}
-          <Route
-            path="*"
-            element={
-              <h1 className="text-blue-500 text-5xl text-center font-bold flex justify-center items-center h-screen">
-                <br />
-                404 Page Not Found
-              </h1>
-            }
-          />{" "}
-          */
-        </Routes>
-      </BrowserRouter>
+                path="*"
+                element={
+                  <h1 className="text-blue-500 text-5xl text-center font-bold flex justify-center items-center h-screen">
+                    <br />
+                    404 Page Not Found
+                  </h1>
+                }
+              />{" "}
+              */
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </>
   );
 }
