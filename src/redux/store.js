9e51c20df9520d909e9f245523fd051e76939ce2
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-24 11:29:58 +0700

    refactor(store): remove commented code for cleaner store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
 // import reducers
 import authReducer from "./slices/authSlice";
 
@@ -19,7 +18,6 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  // Using default middleware (no thunk)
 });
 
 export const persistor = persistStore(store);
